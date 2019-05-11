a6b2914a365fd3852e845478c200e012d62f83bd
Oschart <oscar@aucegypt.edu>
2019-05-11 08:08:08 +0200

    Utility Header File (helper structs)

---
+++ src/DEF_Util.h
@@ -0,0 +1,17 @@
+#ifndef DEFUTIL_H
+#define DEFUTIL_H
+
+#include <utility>
+using namespace std;
+
+
+struct seg
+{
+    int metalLayer, trackIdx;
+    double c1, c2;
+};
+
+// Add whatever utility structs you're going to extract (most importantly, Vias and Metal Layers)
+
+#endif // DEFUTIL_H
+
