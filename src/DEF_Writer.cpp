427517f95929fdecc85eb82f5336ed469b7ba339
Oschart <oscar@aucegypt.edu>
2019-05-20 17:51:16 +0200

    DEF Writer (utilized track offset)

--- src/DEF_Writer.cpp
+++ src/DEF_Writer.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-DEF_Writer::DEF_Writer(vector<metal> _metalStack, vector<vector<seg> > nets)
+DEF_Writer::DEF_Writer(vector<metal> _metalStack, vector<vector<seg>> nets)
 {
     metalStack = _metalStack;
     wireSize();
@@ -20,30 +20,30 @@ DEF_Writer::DEF_Writer(vector<metal> _metalStack, vector<vector<seg> > nets)
             int mIdx = nets[i][j].metalLayer;
             int x1, x2, y1, y2;
             bool same;
-            if(mIdx%2 == 0)     //Horizontal
+            int nextLayer = (mIdx < metalLayers.size() - 1 ? 1 : -1) + mIdx;
+            if (mIdx % 2 == 0) //Horizontal
             {
-                //int centerOffset = nonDefWidth[mIdx]/2;
-                x1 = nets[i][j].c1 + minX;
-                x2 = nets[i][j].c2 + minX;
-                y1 = y2 = nets[i][j].trackIdx + minY;
+                x1 = nets[i][j].c1 + minX + nonDefOffset[nextLayer];
+                x2 = nets[i][j].c2 + minX + nonDefOffset[nextLayer];
+                y1 = y2 = nets[i][j].trackIdx + minY + nonDefOffset[mIdx];
                 same = 0;
             }
-            else        // Vertical
+            else // Vertical
             {
-                x1 = x2 = nets[i][j].trackIdx + minX;
-                y1 = nets[i][j].c1 + minY;
-                y2 = nets[i][j].c2 + minY;
+                x1 = x2 = nets[i][j].trackIdx + minX + nonDefOffset[mIdx];
+                y1 = nets[i][j].c1 + minY + nonDefOffset[nextLayer];
+                y2 = nets[i][j].c2 + minY + nonDefOffset[nextLayer];
                 same = 1;
             }
-            routed[i][j] = "";
+            routed[i][j] = "";      // Make sure it's an empty string
             routed[i][j] += metalStack[mIdx].label + " TAPERRULE LayerScaleRule ";
             routed[i][j] += " ( " + to_string(x1) + " " + to_string(y1) + " ) ";
-            if(x1 == x2 && y1 == y2)
+            if (x1 == x2 && y1 == y2)
             {
                 routed[i][j] += "M" + to_string(mIdx + 2) + "_M" + to_string(mIdx + 1);
-                continue; 
+                continue;
             }
-            routed[i][j] += + " ( " + (same? "* ": to_string(x1)) + (same? to_string(y1): " *") + " ) ";
+            routed[i][j] += +" ( " + (same ? "* " : to_string(x1)) + (same ? to_string(y1) : " *") + " ) ";
         }
     }
 }
@@ -51,10 +51,22 @@ DEF_Writer::DEF_Writer(vector<metal> _metalStack, vector<vector<seg> > nets)
 void DEF_Writer::wireSize()
 {
 
-    for (int i = 0; i < metalStack.size(); i++)
+    nonDefWidth.push_back(metalStack[0].width);
+    nonDefOffset.push_back(metalStack[0].offset);
+    nonDefWidth.push_back(metalStack[1].width);
+    nonDefOffset.push_back(metalStack[1].offset);
+    for (int i = 2; i < metalStack.size(); i++)
     {
-        if(i%2 == 0) nonDefWidth.push_back(metalStack[i].width);
-        else nonDefWidth.push_back(metalStack[i].width * (i + 1) / 2);   
+        if (i % 2 == 0)
+        {
+            nonDefWidth.push_back(metalStack[i].width);
+            nonDefOffset.push_back(metalStack[i].offset);
+        }
+        else
+        {
+            nonDefWidth.push_back(metalStack[i-2].pitch + metalStack[i-2].width);
+            nonDefOffset.push_back(metalStack[i-2].offset + metalStack[i-2].pitch/2);
+        }
     }
 }
 
@@ -77,33 +89,35 @@ void DEF_Writer::write_DEF(string inFile)
             k = 0;
             while (getline(in, token) && token.find("END NETS") == string::npos)
             {
-                
+
                 if (token.find(";") != string::npos)
                 {
                     out << token.substr(0, token.find(";")) << endl;
                     out << "+ ROUTED " << routed[k][0] << endl;
-                    for(int i = 1; i < routed[k].size(); ++i)
+                    for (int i = 1; i < routed[k].size(); ++i)
                     {
                         out << " NEW " + routed[k][i];
-                        if(i == routed[k].size() - 1) out << " ;";
+                        if (i == routed[k].size() - 1)
+                            out << " ;";
                         out << endl;
                     }
                     //
-                    if(k < routed.size()-1)
-                    ++k;
+                    if (k < routed.size() - 1)
+                        ++k;
                 }
                 else
                     out << token << endl;
-            
             }
         }
     }
     out << "NONDEFAULTRULES 1 ;" << endl;
     out << "- LayerScaleRule " << endl;
-    for(int i = 0; i < metalStack.size(); ++i)
+    for (int i = 0; i < metalStack.size(); ++i)
     {
         out << "+ LAYER " + metalStack[i].label + " WIDTH " + to_string(nonDefWidth[i]);
-        if(i == metalStack.size() - 1) out << " ;";
+        out << " OFFSET " + to_string(nonDefOffset[i]);
+        if (i == metalStack.size() - 1)
+            out << " ;";
         out << endl;
     }
     out << "END NONDEFAULTRULES " << endl;
--- src/DEF_Writer.h
+++ src/DEF_Writer.h
@@ -20,7 +20,7 @@ public:
 	
 private:
     vector<metal> metalStack;       // Vector containing metal layers
-    vector<int> nonDefWidth;    // nonDefault Rules
+    vector<int> nonDefWidth, nonDefOffset;    // nonDefault Rules
     vector<seg> wireSeg;        // Wire segments
 	vector<vector<string> > routed;     // ROUTED statements
     void wireSize();        // Size the wires (decide the widths)
